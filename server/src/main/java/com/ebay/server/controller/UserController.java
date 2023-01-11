d7e5d17682af76406d94c8967892e72411bb0df9
dariabuciuman <buciuman.daria@gmail.com>
2023-01-11 19:02:17 +0200

    Optimize more imports

--- server/src/main/java/com/ebay/server/controller/UserController.java
+++ server/src/main/java/com/ebay/server/controller/UserController.java
@@ -2,7 +2,6 @@ package com.ebay.server.controller;
 
 import com.ebay.server.dto.ProductDTO;
 import com.ebay.server.dto.UserDetailsDTO;
-import com.ebay.server.service.ProductService;
 import com.ebay.server.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- server/src/main/java/com/ebay/server/model/User.java
+++ server/src/main/java/com/ebay/server/model/User.java
@@ -1,6 +1,5 @@
 package com.ebay.server.model;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
